f050eebffdf9a74c1bd21d19574484a2af8c84ef
Léo Delmas <ldelmaspro@gmail.com>
2021-10-31 10:57:12 +0100

    Project initialization : wepback, react, compilation

---
+++ README.md
@@ -0,0 +1,28 @@
+# Footview
+
+## 🛠 Prerequisites
+* Node
+
+## 📦 Installation
+
+### ⬇️ Recovery
+```
+git clone https://github.com/leodelmas/footview.git
+cd footview
+```
+
+### 🛠 Initialization
+```
+npm i
+npm run prod
+```
+
+## 💾 Compilation
+* Development
+```
+npm run dev
+```
+* Production
+```
+npm run prod
+```
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, '/public'),
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin()],
+};
